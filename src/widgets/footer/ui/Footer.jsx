bfab1a5a8ee48f49018cfae37d60bed28d07e1cc
MaxatDzhetenbaev <yaboku18@mail.ru>
2024-05-31 09:27:47 +0500

    update: Локализация для footer

--- src/widgets/footer/ui/Footer.jsx
+++ src/widgets/footer/ui/Footer.jsx
@@ -1,8 +1,10 @@
 import { Box, Container, Divider, Typography, useTheme } from "@mui/material";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 export const Footer = () => {
   const theme = useTheme();
+  const { t } = useTranslation();
 
   return (
     <Box sx={{ backgroundColor: theme.palette.primary.main, padding: "20px" }}>
@@ -13,17 +15,10 @@ export const Footer = () => {
             variant="body1"
             sx={{ fontSize: "12px", textAlign: "justify" }}
           >
-            * Информация касательно стоимости, модельного ряда, характеристик,
-            наличия комплектации продукции/автомобиля и наличия опции и/или
-            оборудования в такой комплектации (далее – «Информация»), изложенная
-            на данном сайте и прайс-листах, носит исключительно информативный
-            характер, зависит от местных условий, ограничений и, следовательно
-            может различаться в зависимости от моделей и комплектаций, и не
-            является публичной офертой, согласно ст. 447 Гражданского Кодекса
-            РК.
+            {t("footer_text")}
           </Typography>
           <Divider sx={{ backgroundColor: "#fff", margin: "20px 0" }} />
-          <Typography>© 2024, все права защищены</Typography>
+          <Typography>{t("footer_right")}</Typography>
         </Box>
       </Container>
     </Box>
